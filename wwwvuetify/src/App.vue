85ba947d5c76bb7d1ba5e84a850e019a9e1aded4
Spencer Henze <spencerhenze@gmail.com>
2017-09-29 19:51:09 -0600

    Adds wwwvuetify

---
+++ wwwvuetify/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app dark>
+    <!-- render drawer only if logged in -->
+    <v-navigation-drawer v-if="loggedIn" persistent :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" enable-resize-watcher>
+      <v-list>
+        <v-list-tile value="true" v-for="(item, i) in items" :key="i">
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar fixed>
+      <!-- show drawer icon only if logged in -->
+      <v-toolbar-side-icon v-if="loggedIn" @click.stop="drawer = !drawer" dark></v-toolbar-side-icon>
+
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <router-link v-if="!loggedIn" :to="{name: 'Login'}">
+        <v-btn primary dark>Login</v-btn>
+      </router-link>
+      <v-btn v-if="loggedIn" error dark>Logout</v-btn>
+
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+
+          <!-- Build Main Content Here -->
+          <router-view></router-view>
+          <!-- Build Main Content Here -->
+
+        </v-slide-y-transition>
+      </v-container>
+    </main>
+    <v-footer>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loggedIn: false,
+        drawer: false,
+        items: [
+          { icon: 'bubble_chart', title: 'Inspire' }
+        ],
+        title: 'Keepr'
+      }
+    }
+  }
+
+</script>
---
+++ wwwvuetify/src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="login">
+
+        <v-layout row>
+            <v-flex xs12>
+                <h2>This is where you log in</h2>
+                <v-btn success dark>Login</v-btn>
+            </v-flex>
+        </v-layout>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
